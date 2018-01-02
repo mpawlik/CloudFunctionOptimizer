6368be5f29927f076253927d26fe6a33cef2e7ce
Joanna Kijak <joanna.kijak@u2i.com>
2018-01-02 19:23:21 +0100

    Add levels to tasks in DAG

--- configuration/config.js
+++ configuration/config.js
@@ -1,9 +1,15 @@
 const DAG_PATH = "dag.json";
 const RESULT_DAG_PATH = "decorated-dag.json";
 const STRATEGY_MODULE_PATH = './strategies/montage-strategy';
+const FUNCTION_EXECUTION_TIMES = '0.25.csv';
+const EXECUTION_DATA = 'grafana_data_export.csv';
+const PRICING_DATA = 'gcf_pricing.csv';
 
 module.exports = {
     "path" : DAG_PATH,
     "resultPath" : RESULT_DAG_PATH,
-    "strategyPath" : STRATEGY_MODULE_PATH
+    "strategyPath" : STRATEGY_MODULE_PATH,
+    "functionExecutionTimes" : FUNCTION_EXECUTION_TIMES,
+    "executionData": EXECUTION_DATA,
+    "pricingData": PRICING_DATA
 };
--- strategies/montage-strategy.js
+++ strategies/montage-strategy.js
@@ -1,19 +1,120 @@
 const staticData = require('../csv_reader/read_csv');
-const executionTimesFile = '0.25.csv';
-const executionData = 'grafana_data_export.csv';
-const pricingData = 'gcf_pricing.csv';
+const config = require('../configuration/config');
 
 function montageDecorateStrategy(tasks) {
+
+    // prepare data
+    let functionNames = new Set(tasks.map(task => task.name));
+    let avgExecutionTimes = staticData.avgExecutionTimes(functionNames, config.functionExecutionTimes);
+    // avgExecutionTimes.then(times => console.log(times));
+
+    let totalCost = staticData.totalCost(config.pricingData, config.executionData);
+    totalCost.then(times => console.log(times));
+
+
+    let deadline = 10;
+    let budget = 10;
+
+    //algorithm
+
+    //compute each task rank
+
+    //add levels to tasks
+    let leveledTasks = decorateWithLevels(tasks);
+
+
+
     tasks.forEach(task => {
         task.deploymentType = "2048";
     });
+}
+function getInitialEdges(tasks) {
+  let edges = new Set();
+  tasks.forEach(task => {
+    if (task.name === "mProjectPP") {
+      task.ins.forEach(ins => {
+        edges.add(ins);
+      });
+    }
+  });
+  return edges;
+}
 
-    let functionNames = new Set(tasks.map(task => task.name));
-    let avgExecutionTimes = staticData.avgExecutionTimes(functionNames, executionTimesFile);
-    avgExecutionTimes.then(times => console.log(times));
+function getEdges(tasks, level) {
+  let edges = new Set();
+  tasks.forEach(task => {
+    if (task.level === level) {
+      task.ins.forEach(ins => {
+        edges.add(ins);
+      });
+    }
+  });
+  return edges;
+}
 
-    let totalCost = staticData.totalCost(pricingData, executionData);
-    totalCost.then(times => console.log(times));
+function decorateWithLevels(tasks) {
+
+  let level = 0;
+  let sortedTasks = [];
+  let currentLevelTaskOuts = [];
+
+  let startNode = {
+    "name": "start",
+    "level": level,
+    "outs": []
+  };
+
+  startNode.outs = getInitialEdges(tasks);
+  sortedTasks[0] = startNode;
+  console.log(sortedTasks);
+
+  //get all tasks that have initial edges as ins
+  let nextLevelTasks = new Set();
+  tasks.forEach(task => {
+    task.ins.forEach(ins => {
+      if (startNode.outs.has(ins)) {
+        nextLevelTasks.add(task);
+      }
+    })
+  });
+  // console.log(nextLevelTasks);
+  //add levels
+  level += 1;
+  nextLevelTasks.forEach(task => {
+    task.level = level;
+  });
+
+  //put tasks from that level to sortedTasks
+  sortedTasks[level] = nextLevelTasks;
+
+  //get outs from nextLevelTasks
+  currentLevelTaskOuts = getEdges(nextLevelTasks, level);
+  console.log(sortedTasks);
+
+  //TODO make it in a loop
+
+
+
+  // let currentIns = [];
+  // let currentOuts = [];
+  //
+  // tasks.forEach(task => {
+  //
+  //   let taskIns = task.ins;
+  //   let dependant = false;
+  //
+  //   taskIns.forEach(taskIn => {
+  //     if (currentOuts.includes(taskIn)) {
+  //       dependant = true;
+  //     }
+  //   });
+  //   // console.log(dependant);
+  //
+  //   currentIns = task.ins;
+  //   currentOuts = task.outs;
+  // });
+
+  return tasks;
 }
 
 module.exports = montageDecorateStrategy;
