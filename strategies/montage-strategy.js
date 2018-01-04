aab78e956518e74ef3e170f1f466f516d72e9648
Piotr Martyna <piotrek722@gmail.com>
2018-01-04 17:24:19 +0100

    Added topology ordering, task level decoration and task subdeadline decoration

--- strategies/montage-strategy.js
+++ strategies/montage-strategy.js
@@ -2,22 +2,34 @@ const staticData = require('../csv_reader/read_csv');
 const config = require('../configuration/config');
 const toposort = require('toposort');
 
-function getTopologySortedList(dag) {
+function montageDecorateStrategy(dag) {
 
-    let tasks = dag.tasks;
-    let graph = [];
+    const tasks = dag.tasks;
+
+    const sortedTask = findTopologySortedList(tasks);
+    decorateTasksWithLevels(sortedTask);
+    decorateTasksWithSubDeadline(sortedTask);
+
+    // console.log(sortedTask);
 
     tasks.forEach(task => {
-        let connectedTasks = getEdgesForTask(tasks, task);
+        task.deploymentType = "2048";
+    });
+}
+
+function findTopologySortedList(tasks) {
+
+    const graph = [];
+
+    tasks.forEach(task => {
+        let connectedTasks = findSuccessorsForTask(tasks, task);
         connectedTasks.forEach(connectedTasks => graph.push([task, connectedTasks]))
     });
-    // console.log(toposort(graph));
 
     return toposort(graph);
 }
 
-function getEdgesForTask(tasks, task) {
-
+function findSuccessorsForTask(tasks, task) {
     return tasks.filter(
         filteredTask => filteredTask.ins.some(
             input => task.outs.includes(input)
@@ -25,122 +37,91 @@ function getEdgesForTask(tasks, task) {
     )
 }
 
-function montageDecorateStrategy(dag) {
-
-    getTopologySortedList(dag);
-
-    let tasks = dag.tasks;
-    // prepare data
-    // let functionNames = new Set(tasks.map(task => task.name));
-    // let avgExecutionTimes = staticData.avgExecutionTimes(functionNames, config.functionExecutionTimes);
-    // // avgExecutionTimes.then(times => console.log(times));
-    //
-    // let totalCost = staticData.totalCost(config.pricingData, config.executionData);
-    // totalCost.then(times => console.log(times));
-    //
-    //
-    // let deadline = 10;
-    // let budget = 10;
-    //
-    // //algorithm
-    //
-    // //compute each task rank
-    //
-    // //add levels to tasks
-    // let leveledTasks = decorateWithLevels(tasks);
-
-
-    tasks.forEach(task => {
-        task.deploymentType = "2048";
-    });
+function findPredecessorForTask(tasks, task) {
+    return tasks.filter(
+        filteredTask => task.ins.some(
+            input => filteredTask.outs.includes(input)
+        )
+    )
 }
 
-function getInitialEdges(tasks) {
-    let edges = new Set();
-    tasks.forEach(task => {
-        if (task.name === "mProjectPP") {
-            task.ins.forEach(ins => {
-                edges.add(ins);
-            });
-        }
-    });
-    return edges;
+function findFirstTasks(tasks, ins) {
+    return tasks.filter(
+        task => task.ins.every(
+            input => ins.includes(input)
+        )
+    );
 }
 
-function getEdges(tasks, level) {
-    let edges = new Set();
-    tasks.forEach(task => {
-        if (task.level === level) {
-            task.ins.forEach(ins => {
-                edges.add(ins);
-            });
-        }
-    });
-    return edges;
-}
-
-function decorateWithLevels(tasks) {
-
-    let level = 0;
-    let sortedTasks = [];
-    let currentLevelTaskOuts = [];
-
-    let startNode = {
-        "name": "start",
-        "level": level,
-        "outs": []
-    };
-
-    startNode.outs = getInitialEdges(tasks);
-    sortedTasks[0] = startNode;
-    console.log(sortedTasks);
-
-    //get all tasks that have initial edges as ins
-    let nextLevelTasks = new Set();
-    tasks.forEach(task => {
-        task.ins.forEach(ins => {
-            if (startNode.outs.has(ins)) {
-                nextLevelTasks.add(task);
+function decorateTasksWithLevels(tasks) {
+    tasks.forEach(
+        task => {
+            let predecessors = findPredecessorForTask(tasks, task);
+            if (predecessors.length === 0) {
+                task.level = 1
+            } else {
+                let levels = predecessors.map(pTask => pTask.level);
+                let maxLevel = Math.max(...levels);
+                task.level = maxLevel + 1;
             }
-        })
-    });
-    // console.log(nextLevelTasks);
-    //add levels
-    level += 1;
-    nextLevelTasks.forEach(task => {
-        task.level = level;
-    });
+        }
+    );
+}
 
-    //put tasks from that level to sortedTasks
-    sortedTasks[level] = nextLevelTasks;
+function decorateTasksWithSubDeadline(tasks) {
 
-    //get outs from nextLevelTasks
-    currentLevelTaskOuts = getEdges(nextLevelTasks, level);
-    console.log(sortedTasks);
+    let levelExecutionsMap = new Map();
 
-    //TODO make it in a loop
+    for(let i=1; i <= findTasksMaxLevel(tasks); i++){
 
+        let levelTasks = findTasksFromLevel(tasks, i);
 
-    // let currentIns = [];
-    // let currentOuts = [];
-    //
-    // tasks.forEach(task => {
-    //
-    //   let taskIns = task.ins;
-    //   let dependant = false;
-    //
-    //   taskIns.forEach(taskIn => {
-    //     if (currentOuts.includes(taskIn)) {
-    //       dependant = true;
-    //     }
-    //   });
-    //   // console.log(dependant);
-    //
-    //   currentIns = task.ins;
-    //   currentOuts = task.outs;
-    // });
+        let maxTasksTime = levelTasks.map(
+            ltask => {
+                //todo
+                //get max time for tasks + communication time
+                // if communication time will be in maxTime return just maxTime
+                //ltask.maxTime + Math.max(...findPredecessorForTask(levelTasks, ltask).map(task => task.comTime))
+                return i;
+            }
+        );
 
-    return tasks;
+        let levelExecutionTime = Math.max(...maxTasksTime);
+        levelExecutionsMap.set(i, levelExecutionTime);
+    }
+
+    let totalLevelExecutionTime = 0;
+    for(let value of levelExecutionsMap.values()){
+        totalLevelExecutionTime += value
+    }
+
+    let prevLevelDeadline = 0;
+    for(let i=1; i <= findTasksMaxLevel(tasks); i++){
+
+        //calculate deadline based on prevDeadline, levelExecutionTime and userParameter
+        let levelExecutionTime = levelExecutionsMap.get(i);
+        let subDeadline = calculateSubdeadline(prevLevelDeadline, levelExecutionTime, totalLevelExecutionTime);
+
+        //decorate task at i-th level
+        let levelTasks = findTasksFromLevel(tasks, i);
+        levelTasks.forEach(task => task.subDeadline = subDeadline);
+
+        prevLevelDeadline = subDeadline;
+    }
+}
+
+function calculateSubdeadline(prevLevelDeadline, levelExecutionTime, totalLevelExecutionTime) {
+    //maybe use Math.round(num * 100) / 100 to cut double to 2 decimal places
+    let userDeadlineParameter = 1; //todo get from config
+    return prevLevelDeadline + userDeadlineParameter * ( levelExecutionTime / totalLevelExecutionTime);
+}
+
+function findTasksFromLevel(tasks, level) {
+    return tasks.filter(task => task.level === level);
+}
+
+function findTasksMaxLevel(tasks) {
+    return Math.max(...tasks.map(task => task.level));
 }
 
 module.exports = montageDecorateStrategy;
