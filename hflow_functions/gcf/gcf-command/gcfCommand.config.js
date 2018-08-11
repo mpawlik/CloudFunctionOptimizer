17082c993c55939c9f53d744c4e20b6666fb89a7
Piotr Martyna <piotrek722@gmail.com>
2018-08-11 20:37:54 +0200

    modify run_workflow script

--- hflow_functions/gcf/gcf-command/gcfCommand.config.js
+++ hflow_functions/gcf/gcf-command/gcfCommand.config.js
@@ -1,11 +1,9 @@
-const DEFAULT_URL  = process.env.DEFAULT_URL ? process.env.DEFAULT_URL : "https://us-central1-asia-172718.cloudfunctions.net/hyperflow_executor_128";
-const DEFAULT_RESOURCE = process.env.DEFAULT_RESOURCE ? process.env.DEFAULT_RESOURCE : "128";
+const FUNCTION_TYPE = process.env.FUNCTION_TYPE ? process.env.FUNCTION_TYPE : "2048";
 
 const GOOGLE_BUCKET = "gcf-test123";
 const GOOGLE_PATH   = "data/0.25";
 
-exports.default_url = DEFAULT_URL;
-exports.default_resource = DEFAULT_RESOURCE;
+exports.functionType = FUNCTION_TYPE;
 
 exports.resources = {
     "128": "https://us-central1-asia-172718.cloudfunctions.net/hyperflow_executor_128",
---
+++ scripts/run_workflow.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DAG_PATH=$1
+OUTPUT_DIR=$2
+PROVIDER=$3
+TYPE=$4
+
+#echo Output path: ${TYPE}
+#mkdir ${OUTPUT_DIR}/parsed
+
+# export env variable for hyperflow command
+export FUNCTION_TYPE=${TYPE};
+# run 10 times on default resource
+for i in `seq 10`
+do
+    echo Saving to ${OUTPUT_DIR}/logs_${TYPE}_${i}.txt
+    hflow run ${DAG_PATH} -s >> ${OUTPUT_DIR}/logs_${TYPE}_${i}.txt
+    ./parse_log.sh ${OUTPUT_DIR}/logs_${TYPE}_${i}.txt ${TYPE} ${PROVIDER} >> ${OUTPUT_DIR}/parsed/logs_${TYPE}_${i}.csv
+done
