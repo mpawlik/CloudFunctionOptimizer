705683a4a4d0f3c7e91e8fa094c1ae4030fdc601
Paul Banach <pawel.banach@chargify.com>
2019-02-18 09:01:34 +0100

    Add algorithm comparison

---
+++ plots/algorithm_comparison.R
@@ -0,0 +1,5 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: yoshimori
+# Created on: 18/02/2019
+
---
+++ plots/algorithms_comparison.R
@@ -0,0 +1,17 @@
+setwd('/Users/yoshimori/Studyspace/Magisterka/CloudFunctionOptimizer/plots')
+library('ggplot2')
+
+sdbws_results = read.table("../results/all_x5/real_avg_execution.csv",header = TRUE)
+sdbcs_results = read.table("../results/all_x5_sdbcs/real_avg_execution.csv",header = TRUE)
+
+sdbws_results = sdbws_results[order(sdbws_results$id),]
+sdbws_results$algorithm <- "sdbws"
+
+sdbcs_results = sdbcs_results[order(sdbcs_results$id),]
+sdbcs_results$algorithm <- "sdbcs"
+
+
+results <- rbind(sdbws_results, sdbcs_results)
+ggplot(results, aes(x=id, y=time, group = type, colour=as.factor(algorithm))) + geom_point() + theme(axis.text.x = element_text(angle = 90, hjust = 1)) + labs(x = "Task ID", y = "Time in ms", colour = "Algorithms") + ylim(0, 5000)
+
+
