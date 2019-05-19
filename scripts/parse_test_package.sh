f38e0283fd4baca90c9035025590fcc729025e1a
Maciej Pawlik <m.pawlik@cyfronet.pl>
2019-05-19 17:31:44 +0200

    Add parse_test_package.sh script, parse logs, decorate test dags.

--- scripts/parse_test_package.sh
+++ scripts/parse_test_package.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
+#sample usage:
+# ./scripts/parse_test_package.sh tests/montage_test_package/ tests/montage_test_package_decorated/
+
 scriptdir=`dirname "$0"`
 appdir=`dirname "${scriptdir}"`
 
 inputDir=$1
-outputFolder=$2
+outputDir=$2
 inputFiles=${inputDir}/*
 
 normalizedLogs="./results-request-duration/montage/sdbcs_x5/step1/montage_AWS_256_512_1024_1536_2048_2560_3008x5/normalized_logs.csv"
 
-
 parser=${appdir}/dagscripts/test_workflows/avereage_request_times_decorator.js
 
-mkdir -p ${outputFolder}
+mkdir -p ${outputDir}
 
 for f in ${inputFiles}
 do
     file="$(basename -- $f)"
-    outputFile=${outputFolder}/${file}
-    echo "Parsing file: $file"
-    echo node ${parser} ${f} ${normalizedLogs} ${outputFile}
-#    node ${parser} ${f} ${normalizedLogs} ${outputFile}
+    outputFile=${outputDir}/${file}
+    echo "Processing file: $file"
+    node ${parser} ${f} ${normalizedLogs} ${outputFile}
+    print "Done!"
 done
